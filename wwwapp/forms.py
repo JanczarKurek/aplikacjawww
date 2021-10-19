c08c10a76b8ed0ca985238811fe8922eaaef8622
Janczar Knurek <janczar.kurek@student.uj.edu.pl>
2021-10-19 22:15:56 +0200

    Remove comment
    Signed-off-by: Janczar Knurek <janczar.kurek@student.uj.edu.pl>

--- wwwapp/forms.py
+++ wwwapp/forms.py
@@ -558,25 +558,19 @@ class NewsPostForm(ModelForm):
             css_class='text-right',
         ))
         self.helper.layout = Layout(*layout)
-#
-#
-# class SignupForm(ModelForm):
-#
-#     class Meta:
-#         model = User
-#         fields = ['']
 
 
 class SignUpForm(UserCreationForm):
     username = CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    first_name = CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), max_length=32, help_text='First name')
-    last_name=CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), max_length=32, help_text='Last name')
-    email=EmailField(widget=EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), max_length=64, help_text='Enter a valid email address')
-    password1=CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-    password2=CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
+    first_name = CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+                           max_length=32, help_text='First name')
+    last_name = CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+                          max_length=32, help_text='Last name')
+    email = EmailField(widget=EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+                       max_length=64, help_text='Enter a valid email address')
+    password1 = CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = CharField(widget=PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
 
     class Meta(UserCreationForm.Meta):
         model = User
-        # I've tried both of these 'fields' declaration, result is the same
-        # fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
         fields = UserCreationForm.Meta.fields + ('first_name', 'last_name', 'email',)
